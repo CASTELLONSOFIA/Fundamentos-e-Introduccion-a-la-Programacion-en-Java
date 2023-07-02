f5189a9020eb335dbd7f200b2456afabca1e088b
unknown <202300132@est.umss.edu>
2023-07-02 15:04:42 -0400

    ejercicio unidad 5 subido

---
+++ unidad 5/Persona.java
@@ -0,0 +1,29 @@
+
+public class Persona
+{
+    private String nombre;
+    private int edad, ci;
+    public Persona(String nom, int edad, int ci){
+        nombre=nom;
+        this.edad=edad;
+        this.ci=ci;
+    }
+    public int getCi(){
+        return ci;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setCi(int ci){
+        this.ci=ci;
+    }
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+}
